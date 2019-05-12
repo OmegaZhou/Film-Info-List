e7b63d48ee908e546b466053493d34607bbd20c1
Zhou <playerzhou@icloud.com>
2019-05-12 13:47:01 +0800

    Fix the table property

--- create_table.sql
+++ create_table.sql
@@ -1,6 +1,6 @@
 drop table if exists INFO;
 drop table if exists TITLE;
 drop table if exists CASTS;
-create table INFO(id INTEGER ,film_id INTEGER PRIMARY KEY,film_str VARCHAR(10000));
-create table TITLE(film_id INTEGER PRIMARY KEY,film_title VARCHAR(200));
-create table CASTS(film_id INTEGER,film_cast VARCHAR(300),PRIMARY KEY(film_id,film_cast));
+create table INFO(id INTEGER ,film_id VARCHAR(12) PRIMARY KEY,film_str VARCHAR(10000));
+create table TITLE(film_id VARCHAR(12) PRIMARY KEY,film_title VARCHAR(200));
+create table CASTS(film_id VARCHAR(12),film_cast VARCHAR(300),PRIMARY KEY(film_id,film_cast));
